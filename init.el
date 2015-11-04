409eb2ed7c3974b9e9359aa337233b6310a3d4ff
Tatsuya Ishikawa <ishikawa@jsk.imi.i.u-tokyo.ac.jp>
2015-11-04 13:21:45 +0900

    [.emacs.d] Add function reopen-with-sudo and remove .emacs

--- init.el
+++ init.el
@@ -84,6 +84,14 @@
     (propertize mode-line-str
                 'face '((:foreground "Dark green" :weight bold)))))
 
+(defun reopen-with-sudo ()
+  "Reopen current buffer-file with sudo using tramp."
+  (interactive)
+  (let ((file-name (buffer-file-name)))
+    (if file-name
+        (find-alternate-file (concat "/sudo::" file-name))
+      (error "Cannot get a file name"))))
+
 ;; -*- mode: Emacs-Lisp -*-
 ;;; Global Setting Key
 ;;;
