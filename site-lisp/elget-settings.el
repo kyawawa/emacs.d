88ebd4ef0ea5a41929c30f5e5ccead7eb7740313
Tatsuya Ishikawa <ishikawa@jsk.imi.i.u-tokyo.ac.jp>
2017-10-17 15:45:34 -0400

    [.emacs.d/site-lisp/elget-settings.el] Add lua-mode to autoinstall list

--- site-lisp/elget-settings.el
+++ site-lisp/elget-settings.el
@@ -36,6 +36,7 @@
 (el-get-bundle git-gutter)
 (el-get-bundle git-modes)
 (el-get-bundle jedi-core)
+(el-get-bundle lua-mode)
 (el-get-bundle magit :type github :pkgname "magit/magit" :checkout "93cbf42") ;; ver 1.4.2 for emacs 24.3
 (el-get-bundle markdown-mode)
 (el-get-bundle popwin)
