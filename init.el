e86c5b01987a59a79f95b937c0d9519699796c64
Tatsuya Ishikawa <ishikawa@jsk.imi.i.u-tokyo.ac.jp>
2017-12-05 19:29:09 -0500

    [.emacs.d/init.el] Disable sorting tabbar and load .world as xml-mode

--- init.el
+++ init.el
@@ -438,6 +438,7 @@ This function also returns nil meaning don't specify the indentation."
 
 (setq ring-bell-function 'ignore)
 (setq auto-mode-alist (cons (cons "\\.launch$" 'xml-mode) auto-mode-alist))
+(setq auto-mode-alist (cons (cons "\\.world$" 'xml-mode) auto-mode-alist))
 
 ;; sudo apt-get install rosemacs-el
 (when (getenv "ROS_DISTRO")
@@ -600,18 +601,18 @@ are always included."
   (setq tabbar-select-tab-function 'my-tabbar-buffer-select-tab)
 
   ;; sort by fullpath
-  (defun tabbar-add-tab (tabset object &optional append_ignored)
-  "Add to TABSET a tab with value OBJECT if there isn't one there yet.
- If the tab is added, it is added at the beginning of the tab list,
- unless the optional argument APPEND is non-nil, in which case it is
- added at the end."
-  (let ((tabs (tabbar-tabs tabset)))
-    (if (tabbar-get-tab object tabset)
-        tabs
-      (let ((tab (tabbar-make-tab object tabset)))
-        (tabbar-set-template tabset nil)
-        (set tabset (sort (cons tab tabs)
-                          (lambda (a b) (string< (buffer-name (car a)) (buffer-name (car b))))))))))
+ ;;  (defun tabbar-add-tab (tabset object &optional append_ignored)
+ ;;    "Add to TABSET a tab with value OBJECT if there isn't one there yet.
+ ;; If the tab is added, it is added at the beginning of the tab list,
+ ;; unless the optional argument APPEND is non-nil, in which case it is
+ ;; added at the end."
+ ;;    (let ((tabs (tabbar-tabs tabset)))
+ ;;      (if (tabbar-get-tab object tabset)
+ ;;          tabs
+ ;;        (let ((tab (tabbar-make-tab object tabset)))
+ ;;          (tabbar-set-template tabset nil)
+ ;;          (set tabset (sort (cons tab tabs)
+ ;;                            (lambda (a b) (string< (buffer-name (car a)) (buffer-name (car b))))))))))
 ) ;; end of tabbar settings
 
 ;; ;; auto-complete
