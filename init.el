1662e59bd8ea0542aac9dc8bd2c7be62e84be62c
Tatsuya Ishikawa <ishikawa@jsk.imi.i.u-tokyo.ac.jp>
2018-12-23 16:58:44 +0900

    [.emacs.d/init.el] Emphasize completions first differenct character, and warning face

--- init.el
+++ init.el
@@ -243,6 +243,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(completions-first-difference ((t (:inherit bold :foreground "color-164"))))
+ '(font-lock-warning-face ((t (:foreground "#ff3700"))))
  '(query-replace ((t (:inherit isearch :background "color-40")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
